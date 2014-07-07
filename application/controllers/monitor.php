650c08fafa130d89a37303383780d058060d80a3
Edward <1724996346@qq.com>
2014-07-07 12:09:32 +0800

    增加命令执行功能

--- application/controllers/monitor.php
+++ application/controllers/monitor.php
@@ -54,6 +54,46 @@ class Monitor extends CI_Controller
 		$data['data']['status'] = $this->pack_status();
 		$result = $this->post('http://miner.btckan.com/miner/miner_status', $data);
 
+		$this->status_rsp_proc($result);
+	}
+
+	private function status_rsp_proc($rsp)
+	{
+		$cmd_proc_func = array(
+			'reboot' => 'reboot_cmd_proc',
+			);
+
+		$result = json_decode($rsp, true);
+
+		if(!isset($result['data']['cmd']))
+		{
+			return;
+		}
+
+		$cmd = $result['data']['cmd'];
+		if(!array_key_exists($cmd['operation'], $cmd_proc_func))
+		{
+			echo 'invalid command. ';
+			print_r($cmd);
+			return;
+		}
+
+		$this->$cmd_proc_func[$cmd['operation']]($cmd);
+	}
+
+	private function reboot_cmd_proc($cmd)
+	{
+		$output = array();
+		exec('sudo reboot', $output, $resultCode);
+
+		$data['cmd']['id'] = $cmd['id'];
+		$data['cmd']['resultCode'] = $resultCode;
+		$data['cmd']['result'] = $output;
+
+		$request['head'] = $this->pack_head();
+		$request['data'] = $data;
+
+		$result = $this->post('http://miner.btckan.com/miner/cmd_rsp', $request);
 		echo $result;
 	}
 
